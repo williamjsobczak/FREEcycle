d5a585e52d52897032beb2d0777b8ebac8fa3352
Endrit Berberi <berberi.endrit@ufl.edu>
2024-03-05 15:16:48 -0500

    Added two pages Login and CreateAccount. Pages added on the folder client/src/components. Added respective links in the Homepage

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import CreatePostPage from './pages/CreatePostPage';
+import Login from './components/Login'; // Import the Login component
+import CreateAccount from './components/CreateAccount'; // Import the CreateAccount component
+
 // npm i -D react-router-dom@latest
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />}>
-            <Route index element={<HomePage />} />
-          </Route>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/create_post" element={<CreatePostPage />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/create_post" element={<CreatePostPage />} />
+            <Route path="/login" element={<Login />} /> {/* New route for Login */}
+            <Route path="/create_account" element={<CreateAccount />} /> {/* New route for CreateAccount */}
+          </Routes>
         </Layout>
       </BrowserRouter>
-  
     </div>
   );
 }
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -3,19 +3,32 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-    <Link to="/" className="site-title">
-      FREEcycle
-    </Link>
-    <ul>
-    <Link to="/about" className="site-title">
-      About
-      <ul></ul>
-    </Link>
-    <Link to="/create_post" className="site-title">
-      Create Post
-    </Link>
-    </ul>
-  </nav>
+    <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      <Link to="/" className="text-xl font-bold">
+        FREEcycle
+      </Link>
+      <ul className="flex space-x-4">
+        <li>
+          <Link to="/about" className="hover:text-gray-300">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/create_post" className="hover:text-gray-300">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="hover:text-gray-300">
+            Create Account
+          </Link>
+        </li>
+        <li>
+          <Link to="/create_account" className="hover:text-gray-300">
+            Create Post
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
